02e852fd01da99b2c4c95f08e4200f4063ccca23
curxxed <whaawait0@gmail.com>
2026-04-19 02:28:30 +0300

    HUGE UPDATE: TODO: FINISH THE REST LATER
    0.94a

--- src/main/java/net/curxxed/dev/wintercore/annotation/ForRemoval.java
+++ src/main/java/net/curxxed/dev/wintercore/annotation/ForRemoval.java
@@ -1,5 +1,9 @@
 package net.curxxed.dev.wintercore.annotation;
 
+/**
+ * This is only because Deprecated does not have a forRemoval element in java 8,
+ * and we want to be able to specify the reason for removal.
+ */
 public @interface ForRemoval {
         String value() default "";
 }
